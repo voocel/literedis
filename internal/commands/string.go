7254965b0b3715fe692daf02f278f59b44d6af3c
voocel <voocel@gmail.com>
2024-08-18 15:07:04 +0800

    add storage

--- internal/commands/string.go
+++ internal/commands/string.go
@@ -1,1 +1,23 @@
 package commands
+
+import "literedis/internal/storage"
+
+type String struct {
+	data storage.Storage
+}
+
+func NewString(data storage.Storage) *String {
+	return &String{data: data}
+}
+
+func (s *String) Set(key string, value any) {
+	s.data.Set(key, value)
+}
+
+func (s *String) Get(key string) (any, bool) {
+	return s.data.Get(key)
+}
+
+func (s *String) Del(key string) {
+	s.data.Del(key)
+}
