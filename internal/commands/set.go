c0254ebaf65e70bad46f5b4e6405cc8ebdfd99a4
voocel <voocel@gmail.com>
2024-08-19 23:48:34 +0800

    add storage set

--- internal/commands/set.go
+++ internal/commands/set.go
@@ -1,1 +1,15 @@
 package commands
+
+import "literedis/internal/storage"
+
+type Set struct {
+	data storage.Storage
+}
+
+func NewSet(data storage.Storage) *Set {
+	return &Set{data: data}
+}
+
+func (s *Set) SAdd(key string, value any) {
+	s.data.Set(key, value)
+}
